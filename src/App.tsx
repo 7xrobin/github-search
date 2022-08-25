4b3d579bd941941da7d4a6d74d28936be772ed2c
Robin <carlosrobson0@gmail.com>
2022-08-25 18:28:32 +0200

    Fetch user with react query and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
+import axios from "axios";
+import { useState } from "react";
+import { useQuery } from "react-query";
 import styles from "./App.module.css";
 import Search from "./components/search/search";
 import "./styles/colors.css";
 
+const getUser = async (username: string) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response;
+};
+
 function App() {
+  const [username, setUserName] = useState<string>();
+  const { data, isLoading, refetch } = useQuery(
+    ["getUser", username],
+    () => {
+      username && getUser(username);
+    },
+    { enabled: false }
+  );
+
+  const handleSearch = () => {
+    debugger;
+    refetch();
+  };
+
   return (
     <div className={styles.App}>
       <header>
         <h2>GitHub User Search</h2>
       </header>
-      <Search />
+      <Search onInputChange={setUserName} onSearch={handleSearch} />
     </div>
   );
 }
--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,14 +1,35 @@
+import { ChangeEvent, HTMLProps, useState } from "react";
+import { VoidExpression } from "typescript";
 import { ReactComponent as SearchIcon } from "../../icons/icon-search.svg";
 import styles from "./search.module.css";
 
-interface SearchProps {}
+interface SearchProps extends HTMLProps<HTMLInputElement> {
+  onSearch: () => void;
+  onInputChange: (username: string) => void;
+}
 
-function Search({}: SearchProps) {
+function Search({ onSearch, onInputChange }: SearchProps) {
+  //   const [name, setName] = useState<string>();
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onInputChange(event?.target?.value);
+  };
+
+  const onSearchClick = () => {
+    onSearch();
+  };
+
   return (
     <div className={styles.container}>
       <SearchIcon className={styles.icon} />
-      <input className={styles.input} placeholder="Search Github Username" />
-      <button className={styles.button}>Search</button>
+      <input
+        onChange={handleInputChange}
+        className={styles.input}
+        placeholder="Search Github Username"
+      />
+      <button className={styles.button} onClick={onSearchClick}>
+        Search
+      </button>
     </div>
   );
 }
